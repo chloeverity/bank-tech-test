2c677aded7acf8ace2a83aacb994df031d203939
Chloe Verity <chloeverity@hotmail.co.uk>
2018-12-03 17:16:58 +0000

    deleting BankStatement Class, refactoring logic and making tests pass again

--- src/Account.js
+++ src/Account.js
@@ -3,14 +3,21 @@
 function Account() {
   this.currentBalance = 0;
   this.history = []
+  this.date = getDate()
 }
 
 Account.prototype.deposit = function(amount) {
   this.currentBalance += amount
-  this.history.push({date : new Date, credit : amount, debit : " ", balance : this.currentBalance})
+  this.history.push({date : this.date, credit : amount, debit : " ", balance : this.currentBalance})
 };
 Account.prototype.withdraw = function (amount) {
   this.currentBalance -= amount
-  this.history.push({date : new Date, credit : " ", debit : amount, balance : this.currentBalance})
+  this.history.push({date : this.date, credit : " ", debit : amount, balance : this.currentBalance})
 };
-  var account = new Account()
+function getDate() {
+  let date = new Date();
+  let formatDate = date.getDate() + '/'
+        formatDate += (date.getMonth() + 1) + '/'
+        formatDate += date.getFullYear();
+  return formatDate;
+}
