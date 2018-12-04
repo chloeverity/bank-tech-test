114dc95959afcab70934c70acf4bffb2c5b6caf4
Chloe Verity <chloeverity@hotmail.co.uk>
2018-12-04 10:39:58 +0000

    refactored date so always 10 digits

--- spec/transaction-spec.js
+++ spec/transaction-spec.js
@@ -1,8 +1,10 @@
-// describe("Bank Statement", function() {
-//   it("should test for statement to be logged in console", function () {
-//       console.log = jasmine.createSpy("log");
-//       var account = new Account();
-//       BankStatement.print(account);
-//       expect(console.log).toHaveBeenCalledWith("date || credit || debit || balance");
-//     });
-// })
+describe("Transactions", function() {
+  var transaction;
+
+  beforeEach(function() {
+    transaction = new Transaction("credit", 3000, 3000);
+  })
+  it("date is a property of each transaction", function() {
+    expect(transaction.date.length).toEqual(10)
+  })
+})
