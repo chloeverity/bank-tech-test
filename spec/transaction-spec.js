8b485b6c61a77b70d94650b585f6f156d1c0d753
Chloe Verity <chloeverity@hotmail.co.uk>
2018-12-04 10:44:07 +0000

    adding extra tests to transaction-spec for coverage

--- spec/transaction-spec.js
+++ spec/transaction-spec.js
@@ -7,4 +7,10 @@ describe("Transactions", function() {
   it("date is a property of each transaction", function() {
     expect(transaction.date.length).toEqual(10)
   })
+  it('credit is a property of each transaction', function() {
+    expect(transaction.credit).toEqual(3000)
+  })
+  it('calculates the balance', function() {
+    expect(transaction.currentBalance).toEqual(3000)
+  })
 })
